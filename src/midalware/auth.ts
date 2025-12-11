f9013e8eb0a3f05c54b1cbd7c1fb593687d99cd5
tajul islam <tajulislam199595@gmail.com>
2025-12-11 14:44:08 +0600

    for-recheck

--- src/midalware/auth.ts
+++ src/midalware/auth.ts
@@ -1,39 +1,50 @@
 import { NextFunction, Request, Response } from "express";
-import  jwt, { JwtPayload }  from 'jsonwebtoken';
+import jwt, { JwtPayload } from "jsonwebtoken";
 import { config } from "../config";
 
-const auth=(...roles:string[])=>{
-    return async(req:Request,res:Response,next:NextFunction)=>{
-        try {
-            const token= req.headers.authorization
-            if(!token){
-         res.status(401).json({
+
+const auth = (...roles: string[]) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      let token = req.headers.authorization;
+
+      if (!token) {
+        return res.status(401).json({
           success: false,
           message: "Missing or invalid authentication token",
-        })
-            }
-            
-            const decoded=jwt.verify(token!,config.jwt_secret as string) as JwtPayload
-            // console.log("decoded",decoded);
-            req.user=decoded
-            if(roles.length && !roles.includes(decoded.role)){
-                 res.status(500).json({
-                    success:false,
-                    message:"unauthorized access"
-                })
-            }
-            next()
+        });
+      }
 
-        } catch (error:any) {
-            res.status(400).json({
-                success:false,
-                message:'Validation errors, invalid input',
-                data:error.message
-            })
-        }
+      
+      if (token.startsWith("Bearer ")) {
+        token = token.split(" ")[1];
+      }
 
+      const decoded = jwt.verify(
+        token as string,
+        config.jwt_secret as string
+      ) as JwtPayload;
 
+    
+      (req as any).user = decoded;
+
+      
+      if (roles.length && !roles.includes(decoded.role)) {
+        return res.status(403).json({
+          success: false,
+          message: "Unauthorized access",
+        });
+      }
+
+      return next();
+    } catch (error: any) {
+      return res.status(400).json({
+        success: false,
+        message: "Validation errors, invalid input",
+        data: error.message,
+      });
     }
-}
-export default auth
+  };
+};
 
+export default auth;
